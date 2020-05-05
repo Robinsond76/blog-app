2b51c60f838b0754767a23d2d24beba9c9d76c7b
Robinson Souza <Robinsond76@gmail.com>
2020-05-05 11:35:28 -0400

    Added the NewBlog Component and implemented links in nav

--- src/Components/Blogs/BlogNew.js
+++ src/Components/Blogs/BlogNew.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const BlogNew = () => {
-  return <div className='ui container'>This is the New Blog Page.</div>;
+  return (
+    <div className='ui segment'>
+      <div className='ui huge header'>New Blog</div>
+      <form className='ui form'>
+        <div className='field'>
+          <label>Title</label>
+          <input type='text' name='title' placeholder='Title' />
+        </div>
+        <div className='field'>
+          <label>Image</label>
+          <input type='text' name='img' placeholder='Image' />
+        </div>
+        <div className='field'>
+          <label>Content</label>
+          <textarea name='content'></textarea>
+        </div>
+        <button class='ui button' type='submit'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default BlogNew;
--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='ui inverted menu'>
       <div className='ui container'>
         <div className='header item'>
-          <a href='/'>
+          <Link to='/'>
             <h2>
               <i className='columns icon'></i>Blog App
             </h2>
-          </a>
+          </Link>
         </div>
         <div className='right menu'>
-          <a href='/' className='item'>
-            Home
-          </a>
-          <a href='/blogs/new' className='item'>
-            New Post
-          </a>
+          <div className='item'>
+            <Link to='/'>Home</Link>
+          </div>
+          <div className='item'>
+            <Link to='/blogs/new'>New Post</Link>
+          </div>
         </div>
       </div>
     </div>
